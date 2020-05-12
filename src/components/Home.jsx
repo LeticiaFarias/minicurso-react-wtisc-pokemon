7da1bf199d0763e99efaed38f2a09c6a748d9fc6
Letícia Farias <40062831+LeticiaFarias@users.noreply.github.com>
2020-05-12 00:44:05 -0300

    ✨ Adicionando componentes

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Atividade Pokémon</h1>
+      </div>
+    );
+  }
+}
