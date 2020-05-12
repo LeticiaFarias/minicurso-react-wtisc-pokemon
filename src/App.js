9b8e17118bae9a31118ca36ac0e643677854fba1
Letícia Farias <40062831+LeticiaFarias@users.noreply.github.com>
2020-05-12 00:44:47 -0300

    ✨ Adicionando componentes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./components/Home";
+import Pokedex from "./components/pokedex/Pokedex";
+
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to={"/"} className="navbar-brand">
+              Pokemon WTISC
+            </Link>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mr-auto">
+                <li>
+                  <Link to={"/"} className="nav-link">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/pokedex"} className="nav-link">
+                    Pokédex
+                  </Link>
+                </li>
+                <li>
+                  <span className="nav-link">Pokéball</span>
+                </li>
+                <li>
+                  <span className="nav-link">PokéArena</span>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/pokedex" component={Pokedex} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
